3d53d024f2ef0471821624caea66fc244fed7ebd
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-03-10 14:10:25 +0100

    header

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,28 @@
 <template>
   <div id="app">
-
+    <mainHeader />
   </div>
 </template>
 
 <script>
+import mainHeader from './components/MainHeader.vue';
 
 export default {
+
   name: 'App',
   components: {
+    mainHeader,
+
   }
 }
 </script>
 
 <style lang="scss">
+@import './assets/scss/common.scss';
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Montserrat";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
 }
 </style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,41 @@
+<template>
+
+    <header>
+        <figure class="logo">
+            <img src="../assets/img/spoty.png" alt="">
+        </figure>
+    </header>
+
+</template>
+
+<script>
+
+export default {
+    name: 'mainHeader',
+    
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+@import "../assets/scss/variabili.scss";
+
+header {
+    min-height: 76px;
+    background-color: $colorsec;
+    padding: 10px 20px;
+
+    .logo {
+        width: 46px;
+        height: 46px;
+
+        img {
+            height: 100%;
+
+        }
+    }
+}
+
+    
+</style>
